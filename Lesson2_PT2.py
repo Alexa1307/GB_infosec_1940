544807da4fa99e27ce13592951135e1fc567043e
Alexa1307 <alex.grin1307@rambler.ru>
2022-05-26 18:54:44 +0300

    Убрал лишние пробелы

--- Lesson2_PT2.py
+++ Lesson2_PT2.py
@@ -18,7 +18,7 @@ el = 0
 while i < el_count:
     my_list.append(input("Введите значение списка "))
     i += 1
-for element in range(int(len(my_list)/2)):
-        my_list[el], my_list[el + 1] = my_list [el + 1], my_list[el]
-        el += 2
+for element in range(int(len(my_list) / 2)):
+    my_list[el], my_list[el + 1] = my_list[el + 1], my_list[el]
+    el += 2
 print(my_list)
